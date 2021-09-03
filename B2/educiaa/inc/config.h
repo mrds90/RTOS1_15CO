a7968a182efed57777d819ff6e26d416718f2856
fbuc <fb@inniti.dk>
2021-09-03 17:40:48 -0300

    uploaded ejercicios Bx

---
+++ B2/educiaa/inc/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_h
+#define CONFIG_h
+
+#include <stdio.h>
+#include "sapi.h"
+
+#define EXAMPLE_WELCOME_TEXT    "Ejercicio A.1 en EDUCIAA\n"
+
+#define PRINTF_CONFIGURE
+#define PRINTF(...)             printf(__VA_ARGS__)
+
+#define HACER_FALLAR            0
+
+#endif
